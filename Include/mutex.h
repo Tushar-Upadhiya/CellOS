d8ecf2f0f7b89166816d52383a08152de2d67813
Tushar-Upadhiya <tusharupadhiya2@gmail.com>
2026-09-26 16:05:01 +0530

    Implement blocking mutex with wake-up mechanism (TASK_BLOCKED/TASK_READY transitions)

--- Include/mutex.h
+++ Include/mutex.h
@@ -1,7 +1,9 @@
 #pragma once
 #include<stdint.h>
+#include "task.h"
 typedef struct{
     volatile uint32_t locked;
+    TCB *waiting_task;
 }my_mutex_t;
 
 void mutex_lock(my_mutex_t* m);
--- mutex.c
+++ mutex.c
@@ -1,4 +1,7 @@
 #include "mutex.h"
+#include "task.h"
+
+#define ICSR (*(volatile uint32_t *)0xE000ED04)
 void mutex_lock(my_mutex_t*m){
     while(1){
         __asm volatile("CPSID i");
@@ -7,11 +10,20 @@ void mutex_lock(my_mutex_t*m){
             __asm volatile("CPSIE i");
             return ;
         }
+        
+        current_task->state = TASK_BLOCKED;
+        m->waiting_task=current_task;
         __asm volatile("CPSIE i");
+
+        ICSR |= (1<<28);
     }
 }
 void mutex_unlock(my_mutex_t *m) {
     __asm volatile ("CPSID i");
     m->locked = 0;
+    if(m->waiting_task!=0){
+        m->waiting_task->state = TASK_READY;
+        m->waiting_task = 0;
+    }
     __asm volatile ("CPSIE i");
 }
